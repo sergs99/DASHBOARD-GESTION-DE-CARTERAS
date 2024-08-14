5f133de658522d5ffe1d1fad54948f2029052c1b
sergs99 <sergiosnkrs99@gmail.com>
2024-08-15 00:18:22 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -351,7 +351,7 @@ if menu == "Acciones":
         except Exception as e:
             st.error(f"Ocurrió un error: {e}")
 
-  elif submenu_acciones == "Riesgo":
+  if submenu_acciones == "Riesgo":
     st.subheader("Análisis de Riesgo")
     
     # Entradas de usuario

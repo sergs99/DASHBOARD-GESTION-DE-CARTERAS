46d2bd7fccbc9704c6bb1e592525fa98bae92297
sergs99 <sergiosnkrs99@gmail.com>
2024-08-14 23:29:04 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import streamlit as st
-
+import yfinance as yf
+import pandas as pd
+import matplotlib.pyplot as plt
 # Configuración básica de la aplicación
 st.set_page_config(page_title="Análisis Financiero", layout="wide")
 
